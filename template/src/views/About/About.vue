22f059fe33733b274f4e2fe166746bd3b4cd460a
罗君 <13760614423@163.com>
2019-03-21 11:37:05 +0800

    about

--- template/src/views/About/About.vue
+++ template/src/views/About/About.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class About extends Vue {}
+</script>
+<style lang="less" scoped>
+</style>
